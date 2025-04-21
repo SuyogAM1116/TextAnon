b58a81f8942014c3315b24fec9cf3d6838c91532
Suyog <amsuyog@gmail.com>
2025-04-21 21:20:16 +0530

    Chnaged server.js and videochat.jsx

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -51,8 +51,8 @@ const Chat = () => {
 
         setStatus("Connecting to chat server...");
         setIsConnecting(true);
-        socketRef.current = new WebSocket("ws://localhost:8080");
-        console.log("WebSocket connecting to: ws://localhost:8080");
+        socketRef.current = new WebSocket("wss://textanon.onrender.com");
+        console.log("WebSocket connecting to: wss://textanon.onrender.com");
 
         socketRef.current.onopen = () => {
             console.log("WebSocket onopen: Connected to WebSocket Server");
